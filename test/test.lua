0ec55e457d5e83a9a8b39a637bf52ea83d9af79a
Lewis Dean <ldean@ukcloud.com>
2020-05-28 12:16:48 +0100

    test(numberutil): add tests for fixed/random nums

--- test/test.lua
+++ test/test.lua
@@ -1,15 +1,26 @@
+package.path = package.path .. ";../?.lua"
+
 local lu = require('luaunit')
-print(lu)
+
+local yl_numberutil = require("res.scripts.yl_numberutil")
+local yl_meshlabelutil = require "res.scripts.yl_meshlabelutil"
+
 Tests = {}
 
 function Tests:setUp()
-    self.aValue = 1
+    self.fixedNumber = yl_numberutil.generate("123456")
+    self.randomNumber = yl_numberutil.generate("123xxx")
 end
 
-function Tests:test1()
-    lu.assertEquals(self.aValue, 1)
+function Tests:testFixedNumber()
+    lu.assertStrIContains(self.fixedNumber, "123456")
+end
+
+function Tests:testRandomNumber()
+    lu.assertAlmostEquals(tonumber(self.randomNumber),  123500, 500)
 end
 
 local runner = lu.LuaUnit.new()
 runner:setOutputType("text")
 os.exit( runner:runSuite() )
+
