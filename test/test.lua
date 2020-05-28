352c7092d5896decdd4388e93dbe40abce8f4c94
Lewis Dean <ldean@ukcloud.com>
2020-05-28 14:02:34 +0100

    test(numberutil): add tests for bad params

--- test/test.lua
+++ test/test.lua
@@ -10,10 +10,25 @@ Tests = {}
 function Tests:setUp()
     self.fixedNumber = yl_numberutil.generate("123456")
     self.randomNumber = yl_numberutil.generate("123xxx")
+    self.badRandomNumber = yl_numberutil.generate("12abc34")
+    self.badRandomNumber2 = yl_numberutil.generate("xxabcxx")
+end
+-- numberutil
+
+function Tests:test_Numberutil_FixedNumber()
+    lu.assertStrMatches(self.fixedNumber, "123456")
 end
 
-function Tests:testFixedNumber()
-    lu.assertStrIContains(self.fixedNumber, "123456")
+function Tests:test_Numberutil_RandomNumber()
+    lu.assertAlmostEquals(tonumber(self.randomNumber), 123500, 500)
+end
+
+function Tests:test_Numberutil_BadRandomNumber()
+    lu.assertStrMatches(self.badRandomNumber, "12abc34")
+end
+
+function Tests:test_Numberutil_BadRandomNumber2()
+    lu.assertNotStrContains(self.badRandomNumber2, "xxabcxx")
 end
 
 function Tests:testRandomNumber()
