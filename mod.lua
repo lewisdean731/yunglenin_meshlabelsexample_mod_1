5122ffc31be45228251abb83497ae46b3ea0d542
Lewis Dean <s5008913@bournemouth.ac.uk>
2020-07-06 00:19:41 +0100

    chore: add content for workshop

--- mod.lua
+++ mod.lua
@@ -1,8 +1,22 @@
 function data()
 return {
 	info = {
-		name = _("meshlabels example"),
-		description = _("see readme"),
+	minorVersion = 0, -- minor version, count up from 0
+	severityAdd = "NONE", -- OPTIONAL "NONE", "WARNING" or "CRITICAL"
+	severityRemove = "NONE", -- OPTIONAL "NONE", "WARNING" or "CRITICAL"
+	name = _("Meshlabels"), -- OPTIONAL mod name
+	description = _("see description"), -- OPTIONAL description
+	authors = { -- OPTIONAL one or multiple authors
+		{
+			name = "Yung Lenin", -- author name
+			role = "CREATOR", -- OPTIONAL "CREATOR", "CO_CREATOR", "TESTER" or "BASED_ON" or "OTHER"
+			tfnetId = "" -- OPTIONAL train-fever-net author id
+		}
 	},
+	tags = { "tool", "script" }, -- OPTIONAL "vehicle", "bus", "tram", "train", "steam", "diesel", "electric", "railcar", "wagon", "passenger", "goods", "building", "station", "deco", "town", "depot", "signal", "tool", "pack" or similar tags
+	--tfnetId = {}, -- OPTIONAL train-fever.net download id
+	--dependencies = { }, -- OPTIONAL list of dependent mod ids
+	--url = { }, -- OPTIONAL mod url
+	}
 }
 end
